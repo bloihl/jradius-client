8648b4ff1fe2db1b36f3c7f1f6b99efa132e2018
bloihl <bloihl>
2004-02-03 01:56:37 +0000

    adding comments

--- net/sourceforge/jradiusclient/packets/PapRadiusPacket.java
+++ net/sourceforge/jradiusclient/packets/PapRadiusPacket.java
@@ -3,21 +3,33 @@ package net.sourceforge.jradiusclient.packets;
 import net.sourceforge.jradiusclient.attributes.UserNameAttribute;
 import net.sourceforge.jradiusclient.attributes.UserPasswordAttribute;
 import net.sourceforge.jradiusclient.RadiusAttribute;
+import net.sourceforge.jradiusclient.RadiusAttributeValues;
 import net.sourceforge.jradiusclient.RadiusPacket;
 import net.sourceforge.jradiusclient.exception.InvalidParameterException;
 
 /**
  * Released under the LGPL<BR>
  * @author <a href="mailto:bloihl@users.sourceforge.net">Robert J. Loihl</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
-
 public class PapRadiusPacket extends RadiusPacket {
     private boolean initialized = false;
+    /**
+     * 
+     * @param userName
+     * @param plaintextPassword
+     * @throws InvalidParameterException
+     */
     public PapRadiusPacket(final String userName, final String plaintextPassword )
             throws InvalidParameterException{
         this(userName,plaintextPassword.getBytes());
     }
+    /**
+     * 
+     * @param userName
+     * @param plaintextPassword
+     * @throws InvalidParameterException
+     */
     public PapRadiusPacket(final String userName, final byte[] plaintextPassword )
             throws InvalidParameterException{
         super (ACCESS_REQUEST);
@@ -25,9 +37,13 @@ public class PapRadiusPacket extends RadiusPacket {
         setAttribute(new UserPasswordAttribute(plaintextPassword));
         this.initialized = true;
     }
-    public void validateAttribute(RadiusAttribute ra) throws InvalidParameterException{
-        if ((initialized) && (ra.getType() == RadiusAttribute.USER_NAME ||
-                              ra.getType() == RadiusAttribute.USER_PASSWORD)){
+    /**
+     * 
+     * @param radiusAttribute
+     */
+    public void validateAttribute(RadiusAttribute radiusAttribute) throws InvalidParameterException{
+        if ((initialized) && (radiusAttribute.getType() == RadiusAttributeValues.USER_NAME ||
+            radiusAttribute.getType() == RadiusAttributeValues.USER_PASSWORD)){
             throw new InvalidParameterException ("Already initialized, cannot reset username or password.");
         }
     }
