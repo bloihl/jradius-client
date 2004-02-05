e7485786d031ae1705cf82217d8942a9e70a46a9
bloihl <bloihl>
2004-02-05 18:14:24 +0000

    added method comments

--- net/sourceforge/jradiusclient/attributes/UserNameAttribute.java
+++ net/sourceforge/jradiusclient/attributes/UserNameAttribute.java
@@ -7,14 +7,22 @@ import net.sourceforge.jradiusclient.exception.InvalidParameterException;
 /**
  * Released under the LGPL<BR>
  * @author <a href="mailto:bloihl@users.sourceforge.net">Robert J. Loihl</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 
 public class UserNameAttribute extends RadiusAttribute {
-
+    /**
+     * 
+     * @param username
+     * @throws InvalidParameterException
+     */
     public UserNameAttribute(final String username) throws InvalidParameterException {
         super(RadiusAttributeValues.USER_NAME,username.getBytes());
     }
+    /**
+     * 
+     * @return
+     */
     public String getUserName(){
         return new String(getValue());
     }
--- net/sourceforge/jradiusclient/attributes/UserPasswordAttribute.java
+++ net/sourceforge/jradiusclient/attributes/UserPasswordAttribute.java
@@ -6,13 +6,22 @@ import net.sourceforge.jradiusclient.exception.InvalidParameterException;
 /**
  * Released under the LGPL<BR>
  * @author <a href="mailto:bloihl@users.sourceforge.net">Robert J. Loihl</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class UserPasswordAttribute extends RadiusAttribute {
-
+    /**
+     * 
+     * @param plaintext
+     * @throws InvalidParameterException
+     */
     public UserPasswordAttribute(final String plaintext) throws InvalidParameterException{
         super (RadiusAttributeValues.USER_PASSWORD, plaintext.getBytes());
     }
+    /**
+     * 
+     * @param plaintext
+     * @throws InvalidParameterException
+     */
     public UserPasswordAttribute(final byte[] plaintext) throws InvalidParameterException{
         super (RadiusAttributeValues.USER_PASSWORD, plaintext);
     }
